2456e53f9f8dd0ab9e6d966f5f421efc70f6556d
unknown <bardakov@Bardakov-VRN.reksoft.ru>
2013-12-06 16:19:49 +0400

    logging with log4j

--- src/main/java/ru/vsu/cs/App.java
+++ src/main/java/ru/vsu/cs/App.java
@@ -1,13 +1,20 @@
 package ru.vsu.cs;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+    private static Logger logger = LogManager.getLogger("HelloWorld");
+
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        logger.info("Program run...");
     }
 }
